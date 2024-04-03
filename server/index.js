d1dcc215237a8fd3e40c59c4b942bea5ad58601b
ehteshamtarq <ehteshamtarique1@gmail.com>
2024-04-03 13:48:24 +0530

    morgan package added

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
+require("dotenv").config();
 const PORT = process.env.PORT || 5000
 const express = require('express');
+const morgan = require("morgan");
 const cors = require('cors');
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
 app.listen(PORT, ()=> console.log('Your server is running on PORT '+PORT));
 
-const API_KEY = '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';
+const API_KEY = process.env.API_KEY;
 app.post('/completions', async (req, res)=>{
     const options ={
         method:"POST",
@@ -23,7 +26,6 @@ app.post('/completions', async (req, res)=>{
     try{
         const response = await fetch('https://api.openai.com/v1/chat/completions', options);
         const data = await response.json();
-        console.log(data);
         res.send(data);
     }
     catch(error){
