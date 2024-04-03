8a634f7238510f860c1e49a95798c6ebfe11bd61
EHTESHAM TARIQUE <72429244+ehteshamtarq@users.noreply.github.com>
2024-04-03 13:21:34 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 app.listen(PORT, ()=> console.log('Your server is running on PORT '+PORT));
 
-const API_KEY = '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';
+const API_KEY = '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';
 app.post('/completions', async (req, res)=>{
     const options ={
         method:"POST",
